60db0b3e4ec0ed42576c415a9d78d6550c360b7c
RobotMan2412 <julian@scheffers.net>
2026-09-03 17:26:35 +0200

    Initgraph

--- src/init/init.cpp
+++ src/init/init.cpp
@@ -5,8 +5,54 @@
 
 namespace init {
 
+extern "C" Task __start_initgraph;
+extern "C" Task __stop_initgraph;
+
 void init() {
     log::printf("Kernel starting...\n");
+
+    bool progress = false;
+    bool work     = true;
+    while (work) {
+        progress = false;
+        work     = false;
+        for (auto task = &__start_initgraph; task != &__stop_initgraph; task++) {
+            if (!task->finished) {
+                bool can_run = true;
+                for (std::size_t i = 0; i < task->deps_len; i++) {
+                    if (!task->deps[i]->finished) {
+                        can_run = false;
+                        break;
+                    }
+                }
+                if (can_run) {
+                    if (task->func) {
+                        log::printf("Task %s...", task->name);
+                        task->func();
+                        log::printf(" Done.\n");
+                    } else {
+                        log::printf("Reached stage %s\n", task->name);
+                    }
+                    task->finished = true;
+                    progress       = true;
+                } else {
+                    work = true;
+                }
+            }
+        }
+
+        if (work && !progress) {
+            log::printf("Initgraph deadlock detected:\n");
+            for (auto task = &__start_initgraph; task != &__stop_initgraph; task++) {
+                if (!task->finished) {
+                    log::printf("  %s\n", task->name);
+                }
+            }
+            while (1);
+        }
+    }
+
+    log::printf("Kernel initialized.\n");
 }
 
 } // namespace init
--- src/init/init.hpp
+++ src/init/init.hpp
@@ -1,9 +1,32 @@
 
 #pragma once
 
+#include <cstddef>
+
+
+
 namespace init {
+
+// An initgraph task.
+struct Task {
+    char const *name;
+    void (*func)();
+    Task *const *deps;
+    std::size_t  deps_len;
+    bool         finished;
+};
 
 // Initialize the kernel and start userspace.
 void init();
+
+// Declare an initgraph task with an optional function to run when all dependencies are met.
+#define INITGRAPH_TASK(name_, func_, ...)                                                                              \
+    [[gnu::section(".initgraph"), gnu::used]] ::init::Task name_ = {                                                   \
+        .name     = #name_,                                                                                            \
+        .func     = (func_),                                                                                           \
+        .deps     = (Task *const[]){__VA_ARGS__},                                                                      \
+        .deps_len = sizeof((Task *const[]){__VA_ARGS__}) / sizeof(Task *),                                             \
+        .finished = false,                                                                                             \
+    };
 
 } // namespace init
